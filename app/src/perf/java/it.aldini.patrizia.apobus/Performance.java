68bc70cd7e9a6b4c3edc76ea2bd28113881a0d3d
patriziamartemucci <martemucci.patrizia@gmail.com>
2016-07-29 15:00:16 +0200

    prova
    Nuovo progetto di prova

---
+++ app/src/perf/java/it.aldini.patrizia.apobus/Performance.java
@@ -0,0 +1,13 @@
+package it.aldini.patrizia.apobus;
+
+/**
+ * Created by Massimo Carli on 02/08/15.
+ */
+public class Performance {
+
+
+    public void method(){
+        System.out.println("URL " + BuildConfig.PERF_URL);
+    }
+
+}
